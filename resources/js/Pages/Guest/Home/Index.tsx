54aec7986db22c52233ab57acd0cddef223e1530
Ben Gunn <bengunn.dev@gmail.com>
2024-08-20 18:18:00 +0700

    frontend: landing page

--- resources/js/Pages/Guest/Home/Index.tsx
+++ resources/js/Pages/Guest/Home/Index.tsx
@@ -1,11 +1,27 @@
 import { Head } from '@inertiajs/react';
 import Layout from '@/Pages/Guest/Partials/Layout';
+import Hero from './Partial/Hero';
+import { Input } from '@/components/ui/input';
+import SelectFlight from './Partial/SelectFlight';
+import { Button } from '@/components/ui/button';
 
 export default function Index() {
     return (
         <Layout>
             <Head title="Home" />
-            <h1>Home Page</h1>
+            <Hero />
+            <div className="flex justify-center">
+                <div className="w-full md:w-5/6 p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
+                    <div className="grid grid-cols-1 space-y-2 md:space-y-0 md:grid-cols-3 sm:gap-4">
+                        <SelectFlight />
+                        <SelectFlight />
+                        <Input type="date" className="h-12" />
+                    </div>
+                    <div className="flex justify-center mt-8">
+                        <Button className="h-12 w-1/2" >Search</Button>
+                    </div>
+                </div>
+            </div>
         </Layout>
     );
 }
---
+++ resources/js/Pages/Guest/Home/Partial/Hero.tsx
@@ -0,0 +1,9 @@
+export default function Hero() {
+    return (
+        <div className="my-4 sm:my-8">
+            <p className="mb-2 text-center font-semibold text-teal-600 md:mb-3 lg:text-lg">Introducing</p>
+            <h2 className="mb-2 text-center text-2xl font-bold lg:text-3xl">Secure Flight System</h2>
+            <p className="mx-auto max-w-screen-md text-center md:text-lg">Lorem Ipsum is simply dummy text of the printing and typesetting industry. </p>
+        </div>
+    );
+}
